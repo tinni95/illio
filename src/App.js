8bb702786cf736e59f9e74627865f5732fbefe78
tinni95 <g.damico241095@gmail.com>
2021-04-26 19:35:16 +0200

    adding spinner

--- src/App.js
+++ src/App.js
@@ -2,38 +2,49 @@ import "./App.css";
 import InputCol from "./components/inputCol";
 import InputDateCol from "./components/inputDateCol";
 import moment from "moment";
-
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import "react-datepicker/dist/react-datepicker.css";
 import { useEffect, useState } from "react";
 import LineChart from "./components/lineChart";
 import axios from "axios";
 import { GET_STOCK } from "./constants/api";
+import Loader from "react-loader-spinner";
+
 function App() {
   const [startDate, setStartDate] = useState(new Date("2020-01-01"));
   const [endDate, setEndDate] = useState(new Date("2021-01-01"));
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      var config = {
-        method: "get",
-        url: GET_STOCK(
+      setLoading(true);
+      const { data } = await axios.get(
+        GET_STOCK(
           moment(startDate).format("YYYY-MM-DD"),
           moment(endDate).format("YYYY-MM-DD")
-        ),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-
-      const { data } = await axios(config);
-      console.log(data);
+        )
+      );
       setData(data);
+      setLoading(false);
     };
 
     fetchData();
   }, [endDate, startDate]);
 
+  if (loading) {
+    return (
+      <div className="App">
+        <Loader
+          type="Puff"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          timeout={3000} //3 secs
+        />
+      </div>
+    );
+  }
   return (
     <div className="App">
       <div className="inputRow">
